1fccaeb977cabfb349a5def32efe0478ef4270b3
Edgar Vaguencia <isc.edgarvalencia@gmail.com>
2017-03-08 14:00:09 -0600

    Http implements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes.component';
@@ -11,14 +11,25 @@ import { DashboardComponent } from './dashboard.component';
 
 import { AppRoutingModule } from './app-routing.module';
 
+import { HeroSearchComponent } from './hero-search.component';
+import { HeroSearchService } from './hero-search.service';
+
+// Fake import loading & configuring in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
 @NgModule({
   declarations: [
     DashboardComponent,
     HeroesComponent,
     HeroDetailComponent,
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroSearchComponent
   ],
   imports: [
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     BrowserModule,
     FormsModule,
     AppRoutingModule
